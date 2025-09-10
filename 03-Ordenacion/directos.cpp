17d694b3370bd6358e974f82f07215d089fc99af
Awar <amidwar05@gmail.com>
2025-09-09 19:23:20 -0500

    Pidiendo numeros y guardando

---
+++ 03-Ordenacion/directos.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+
+//Prototipo de la funcion
+//void interDirDer(int arr[], int );
+
+int main(){
+	
+	int arr[100];
+	int n;
+	
+	cout<<"Digite el tamaño del arreglo: "; cin>>n;
+	for(int i=0; i<n; i++){
+		cout<<"Digite el numero: "; 
+		cin>>arr[i];
+	}
+	
+	//interDirDer(arr,n);
+	
+	
+	
+	return 0;
+}
+
+//void interDirDer(int arr[], int );
+
